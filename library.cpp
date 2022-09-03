783c1304453f2352444c1166247b1a78e8ac397f
iamkroot <kroot.patel@gmail.com>
2022-09-03 15:18:16 +0530

    Fib in cpp

--- library.cpp
+++ library.cpp
@@ -1,12 +1,21 @@
 #include "com_craftinginterpreters_lox_Lox.h"
 #include <fmt/core.h>
 
-jlong Java_com_craftinginterpreters_lox_Lox_compileJit(JNIEnv*, jclass) {
+extern "C" int fib(int n) {
+    if (n < 2) return n;
+    return fib(n - 2) + fib(n - 1);
+}
+
+[[maybe_unused]] jlong Java_com_craftinginterpreters_lox_Lox_compileJit(JNIEnv*, jclass) {
     fmt::print("Compiling jit!\n");
     return 123;
 }
 
-jint Java_com_craftinginterpreters_lox_Lox_runJit(JNIEnv*, jclass, jlong addr, jint arg) {
+[[maybe_unused]] jint Java_com_craftinginterpreters_lox_Lox_runJit(JNIEnv*, jclass, jlong addr, jint arg) {
     fmt::print("Running jit! {} {}\n", addr, arg);
-    return 0;
+    std::fflush(stdout);
+    auto val = fib(arg);
+    fmt::print("Got val {}\n", val);
+    std::fflush(stdout);
+    return val;
 }
