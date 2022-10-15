dedaceb89a879ba164bd9da0a4b3a039bd059ffa
iamkroot <kroot.patel@gmail.com>
2022-10-15 15:22:14 +0530

    Fix fib.c

--- fib.c
+++ fib.c
@@ -2,10 +2,13 @@ int fib2(int n);
 
 int fib(int n) {
     return fib2(n);
+    // if (n < 2)
+    //     return n;
+    // return fib(n-1) + fib(n-2);
 }
 
 int sub1(int n) {
-    return n-2;
+    return n-1;
 }
 
 int lt2(int n) {
@@ -16,8 +19,8 @@ int fib1(int n) {
     int isless = lt2(n);
     if (isless)
         return n;
-    int n2 = sub1(n) - 1;
-    return fib1(n2) + fib2(sub1(n));
+    int n2 = sub1(n);
+    return fib1(n2) + fib2(sub1(n) - 1);
 }
 
 int fib2(int n) {
